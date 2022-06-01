2314c775f14fd011629ff3b1d84055c1acfdd76e
Utkarsh Singh <sutkarsh453@gmail.com>
2022-06-02 00:44:45 +0530

    Add query selectors

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ const popup = document.querySelector('.popup');
 const container = document.querySelector('.container');
 const add = document.querySelector('.addBook');
 const closeBtn = document.querySelector('#close');
+const submit = document.querySelector('.submit');
+const delAll = document.querySelector('.deleteAll');
+const del = document.querySelector('.del');
 
 add.addEventListener('click', addBook);
 closeBtn.addEventListener('click', function(){
