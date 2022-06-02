398d5f9b1954a1b2d82126be35e56489a4617de8
Utkarsh Singh <sutkarsh453@gmail.com>
2022-06-02 14:06:50 +0530

    Add functions for btns

--- script.js
+++ script.js
@@ -7,23 +7,81 @@ function Book(title, author, pages, readOrNot){
     this.readOrNot = readOrNot;
 }
 
-function addBookToLibrary(){
-    
-}
-
 function addBook(){
     popup.classList.add("active");
+}
+
+function removeBox(){
+    const delBtn = document.querySelectorAll('.del');
+    console.log(delBtn)
+    delBtn.forEach(btn => {
+        btn.addEventListener('click', function(e){
+            
+            content.removeChild(content.children[ind]);
+        })
+    });
 }
 
 const popup = document.querySelector('.popup');
 const container = document.querySelector('.container');
 const add = document.querySelector('.addBook');
 const closeBtn = document.querySelector('#close');
-const submit = document.querySelector('.submit');
 const delAll = document.querySelector('.deleteAll');
 const del = document.querySelector('.del');
+const title = document.querySelector('#title')
+const author = document.querySelector('#author')
+const pages = document.querySelector('#pages')
+const rON = document.querySelector('#rON')
+const form = document.querySelector('.form')
+const content = document.querySelector('.content')
 
 add.addEventListener('click', addBook);
+
 closeBtn.addEventListener('click', function(){
     popup.classList.remove('active');
 });
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    popup.classList.remove('active');
+    let book = new Book(title.value, author.value, pages.value, rON.checked);
+    myLibrary.push(book);
+
+    const box = document.createElement('div')
+    box.classList.add('box');
+    const info1 = document.createElement('div')
+    const info2 = document.createElement('div')
+    const info3 = document.createElement('div')
+    const read = document.createElement('button')
+    const del = document.createElement('button')
+    info1.classList.add('info1');
+    info2.classList.add('info2');
+    info3.classList.add('info3');
+    read.classList.add('read');
+    del.classList.add('del');
+
+    info1.textContent = title.value;
+    info2.textContent = author.value;
+    info3.textContent = pages.value;
+    if(rON.checked == true){
+        read.textContent = 'Read';
+    }
+    else{
+        read.textContent = 'Not Read';
+    }
+    del.textContent = 'Remove';
+
+    box.appendChild(info1)
+    box.appendChild(info2)
+    box.appendChild(info3)
+    box.appendChild(read)
+    box.appendChild(del);
+    content.appendChild(box);
+
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    rON.checked = false;
+
+    removeBox();
+})
+
